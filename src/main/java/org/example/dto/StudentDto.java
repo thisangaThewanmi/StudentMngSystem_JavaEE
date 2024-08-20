e3605138a1297b0a1945acff7a161806d8b2c363
thisangaThewanmi <thisangat2005@gmail.com>
2024-08-20 20:11:56 +0530

    created the dto and entity

--- src/main/java/org/example/dto/StudentDto.java
+++ src/main/java/org/example/dto/StudentDto.java
@@ -1,4 +1,18 @@
 package org.example.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class StudentDto {
+    private String id;
+        private String name;
+        private String city;
+        private String email;
+        private String Level;
+
 }
--- src/main/java/org/example/entity/Student.java
+++ src/main/java/org/example/entity/Student.java
@@ -1,4 +1,18 @@
 package org.example.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Student {
+
+    private String id;
+    private String name;
+    private String city;
+    private String email;
+    private String Level;
 }

a55bcd2c8489ccfa7db0ff787feae5112c378a26
thisangaThewanmi <thisangat2005@gmail.com>
2024-08-22 14:01:47 +0530

    Created CROS origin issue by developing CROSFilter

--- src/main/java/org/example/filter/CORSFilter.java
+++ src/main/java/org/example/filter/CORSFilter.java
@@ -9,25 +9,30 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/*")
+@WebFilter(urlPatterns = "/*")//meken kiyanne ena request hama eakatama meka apply wenawa kila ekai wild card mapping ekak daa tiyenne mkk awath meka apply wenna ona
 public class CORSFilter  extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
 
-        var origin = req.getHeader("Origin");
-        var configuredOrigin = getServletContext().getInitParameter("origin");
+        var origin = req.getHeader("Origin"); // ena request eke header eka gannawa (web.xml ekedi api header eka denawa)
+        var configuredOrigin = getServletContext().getInitParameter("origin");//meken dd eken dena origin eka gannawa
 
         if(origin !=null && (configuredOrigin == null || origin.contains(configuredOrigin))) {
 
+            // allowing the request's origin to access the resource.
             res.setHeader("Access-Control-Allow-Origin", origin);
 
+            //acess eka denna ona methods
             res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
+            //allow karann ona heaers
             res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+            //expose karana oa headers mnwada
             res.setHeader("Access-Control-Expose-Headers", "Content-Type");
 
+            // allowing cookies and other credentials to be included in CORS requests.
             res.setHeader("Access-Control-Allow-Credentials","true");
 
         }
